f6aa5656f5dd3a77bbcc0ca461097fde9e86f08c
Eduardo Munoz <02eduardo10@gmail.com>
2024-04-18 17:33:16 -0400

    Practicando .js

---
+++ script.js
@@ -0,0 +1,72 @@
+class Calculadora {
+    constructor() {
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.operacion = undefined;
+    }
+
+    agregarNumero(numero) {
+        if (numero === '.' && this.valorActual.includes('.')) return;
+        this.valorActual = this.valorActual.toString() + numero.toString();
+        this.actualizarDisplay();
+    }
+
+    operar(operacion) {
+        if (this.valorActual === '') return;
+        if (this.valorAnterior !== '') {
+            this.calcular();
+        }
+        this.operacion = operacion;
+        this.valorAnterior = this.valorActual;
+        this.valorActual = '';
+        this.actualizarDisplay();
+    }
+
+    calcular() {
+        let calculo;
+        const anterior = parseFloat(this.valorAnterior);
+        const actual = parseFloat(this.valorActual);
+        if (isNaN(anterior) || isNaN(actual)) return;
+
+        switch (this.operacion) {
+            case '+':
+                calculo = anterior + actual;
+                break;
+            case '-':
+                calculo = anterior - actual;
+                break;
+            case '*':
+                calculo = anterior * actual;
+                break;
+            case '%':
+                calculo = anterior % actual;
+                break;
+            default:
+                return;
+        }
+
+        this.valorActual = calculo;
+        this.operacion = undefined;
+        this.valorAnterior = '';
+        this.actualizarDisplay();
+    }
+
+    borrar() {
+        this.valorActual = this.valorActual.toString().slice(0, -1);
+        this.actualizarDisplay();
+    }
+
+    borrarTodo() {
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.operacion = undefined;
+        this.actualizarDisplay();
+    }
+
+    actualizarDisplay() {
+        document.getElementById('valor-actual').innerText = this.valorActual;
+        document.getElementById('valor-anterior').innerText = this.valorAnterior + ' ' + (this.operacion || '');
+    }
+}
+
+const calculadora = new Calculadora();
